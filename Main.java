f9a70c39ce94d7dcd96dd6bcb15a35bb1c62c618
ir2020 <indrek.runkaru@khk.ee>
2020-10-27 20:27:45 +0200

    Excercise 65 average of numbers

--- Main.java
+++ Main.java
@@ -1,26 +1,31 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void removeLast(ArrayList<String> list){
-        int numberofListItems = 0;
-
-        for (String word : list) {
-            numberofListItems++;
+    public static int sum(ArrayList<Integer> list) {
+        // write your code here
+        Integer sum = 0;
+        for (int numb : list) {
+            sum = sum + numb;
         }
-        numberofListItems = numberofListItems - 1;
-        list.remove(numberofListItems);
+        return sum;
+    }
+    public static double average(ArrayList<Integer> list) {
+        Integer impoNumber = sum(list);
+        Double howManyNumbers = 0.0;
+        for(Integer number : list){
+            howManyNumbers++;
+        }
+        double result = impoNumber/howManyNumbers;
+        return result;
     }
     public static void main(String[] args) {
-        ArrayList<String> brothers = new ArrayList<String>();
-        brothers.add("Dick");
-        brothers.add("Henry");
-        brothers.add("Michael");
-        brothers.add("Bob");
-        System.out.println("brothers:");
-        System.out.println(brothers);
-// sorting brothers
-// removing the last item
-        removeLast(brothers);
-        System.out.println(brothers);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+        System.out.println(sum(list));
+        System.out.println(average(list));
+        System.out.println("The average is: " + average(list));
     }
 }
